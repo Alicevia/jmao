9e912fd981bf851e72e8c8b567b2679a2fd3b97d
Alicevia <ed-phoenix@qq.com>
2019-11-28 18:35:41 +0800

    解决antd table

--- src/views/home/navRouer/productInfo/productInfo.vue
+++ src/views/home/navRouer/productInfo/productInfo.vue
@@ -9,7 +9,7 @@
     </a-input-search>
    </div>
     <a-table :rowSelection="rowSelection" :columns="columns" 
-    :scroll="{x:'120%',y:570}" :size='"middle"' rowKey='id'
+    :scroll="{x:1500,y:570}"  rowKey='id'
     :pagination='{
       defaultPageSize:20,
       size:"middle",
@@ -23,20 +23,20 @@
 
 <script>
   const columns = [
-    {width:'5%',fixed:'left', title: '车系',  dataIndex: 'seriesName', key: 'seriesName', align: 'center'},
-    {width:'5%',fixed:'left', title: '车型',  dataIndex: 'vehicleName', key: 'vehicleName',align: 'center'},
-    {width:'5%',fixed:'left', title: 'OE号',  dataIndex: 'oenumber', key: 'oenumber',align: 'center'},
-    {width:'5%',fixed:'left', title: '吉茂品号',  dataIndex: 'jiMaoNumber', key: 'jiMaoNumber',align: 'center'},
-    {width:'5%',fixed:'left', title: '汽车年份(起)',  dataIndex: 'beginningYear', key: 'beginningYear',align: 'center'},
-    {width:'5%',fixed:'left', title: '汽车年份(止)',  dataIndex: 'endYear', key: 'endYear',align: 'center'},
-    {width:'5%',fixed:'left', title: '排量',  dataIndex: 'displacement', key: 'displacement',align: 'center'},
-    {width:'5%',fixed:'left', title: '气缸数',  dataIndex: 'cylinderNumber', key: 'cylinderNumber',align: 'center'},
-    {width:'5%',fixed:'left', title: '自动/手动',  dataIndex: 'gearPosition', key: 'gearPosition',align: 'center'},
-    {width:'5%',fixed:'left', title: '水箱材质',  dataIndex: 'tankMaterial', key: 'tankMaterial',align: 'center'},
-    {width:'5%',fixed:'left', title: '蕊体尺寸',  dataIndex: 'pistilSize', key: 'pistilSize',align: 'center'},
-    {width:'5%',fixed:'left', title: '蕊厚',  dataIndex: 'pistil', key: 'pistil',align: 'center'},
-    {width:'5%',fixed:'left', title: '波高',  dataIndex: 'waveformHeight', key: 'waveformHeight',align: 'center'},
-    {width:'5%',fixed:'left',
+    {width:100,fixed:'left', title: '车系',  dataIndex: 'seriesName', key: 'seriesName', align: 'center'},
+    {width:100,fixed:'left', title: '车型',  dataIndex: 'vehicleName', key: 'vehicleName',align: 'center'},
+    {width:100, title: 'OE号',  dataIndex: 'oenumber', key: 'oenumber',align: 'center'},
+    {width:100, title: '吉茂品号',  dataIndex: 'jiMaoNumber', key: 'jiMaoNumber',align: 'center'},
+    {width:150, title: '汽车年份(起)',  dataIndex: 'beginningYear', key: 'beginningYear',align: 'center'},
+    {width:150, title: '汽车年份(止)',  dataIndex: 'endYear', key: 'endYear',align: 'center'},
+    {width:100, title: '排量',  dataIndex: 'displacement', key: 'displacement',align: 'center'},
+    {width:100, title: '气缸数',  dataIndex: 'cylinderNumber', key: 'cylinderNumber',align: 'center'},
+    {width:100, title: '自动/手动',  dataIndex: 'gearPosition', key: 'gearPosition',align: 'center'},
+    {width:100, title: '水箱材质',  dataIndex: 'tankMaterial', key: 'tankMaterial',align: 'center'},
+    {width:100, title: '蕊体尺寸',  dataIndex: 'pistilSize', key: 'pistilSize',align: 'center'},
+    {width:100, title: '蕊厚',  dataIndex: 'pistil', key: 'pistil',align: 'center'},
+    {width:100, title: '波高',  dataIndex: 'waveformHeight', key: 'waveformHeight',align: 'center'},
+    {width:100,fixed:'right',
       title: 'Action',
       key: 'operation',
       scopedSlots: { customRender: 'action' },
@@ -68,6 +68,37 @@
         "waveformHeight": i
       });
   }
+
+  // const columns = [
+  //   { title: 'Full Name', width: 100, dataIndex: 'name', key: 'name', fixed: 'left' },
+  //   { title: 'Age', width: 100, dataIndex: 'age', key: 'age', fixed: 'left' },
+  //   { title: 'Column 1', dataIndex: 'address', key: '1', width: 150 },
+  //   { title: 'Column 2', dataIndex: 'address', key: '2', width: 150 },
+  //   { title: 'Column 3', dataIndex: 'address', key: '3', width: 150 },
+  //   { title: 'Column 4', dataIndex: 'address', key: '4', width: 150 },
+  //   { title: 'Column 5', dataIndex: 'address', key: '5', width: 150 },
+  //   { title: 'Column 6', dataIndex: 'address', key: '6', width: 150 },
+  //   { title: 'Column 7', dataIndex: 'address', key: '7', width: 150 },
+  //   { title: 'Column 8', dataIndex: 'address', key: '8' },
+  //   {
+  //     title: 'Action',
+  //     key: 'operation',
+  //     fixed: 'right',
+  //     width: 100,
+  //     scopedSlots: { customRender: 'action' },
+  //   },
+  // ];
+
+  // const data = [];
+  // for (let i = 0; i < 100; i++) {
+  //   data.push({
+  //     key: i,
+  //     name: `Edrward ${i}`,
+  //     age: 32,
+  //     address: `London Park no. ${i}`,
+  //   });
+  // }
+
 export default {
   data() {
     return {

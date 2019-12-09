58e41ed5415671f27d41f1aa0f5721195714866a
Alicevia <ed-phoenix@qq.com>
2019-12-09 20:52:21 +0800

    finish

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
-  publicPath:'/',
+  publicPath:'./',
   chainWebpack: (config) => {
     config.resolve.alias
       .set('@', resolve('src'))
